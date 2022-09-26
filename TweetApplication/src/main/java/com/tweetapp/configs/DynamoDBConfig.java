09c88316440e58a7f74428ffe085db1ccef402c6
KazamiSkyress <85171739+KazamiSkyress@users.noreply.github.com>
2022-09-26 15:57:08 +0530

    Add files via upload

---
+++ TweetApplication/src/main/java/com/tweetapp/configs/DynamoDBConfig.java
@@ -0,0 +1,32 @@
+package com.tweetapp.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+
+@Configuration
+public class DynamoDBConfig {
+	
+	public static final String SERVICE_ENDPOINT = "https://dynamodb.ap-south-1.amazonaws.com";
+	public static final String REGION = "ap-south-1";
+	public static final String ACCESS_KEY = "AKIASNYYNSNQOZRL6KOB";
+	public static final String SECRET_KEY = "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";
+
+	@Bean
+	public DynamoDBMapper mapper() {
+		return new DynamoDBMapper(amazonDynamoDBConfig());
+	}
+
+	private AmazonDynamoDB amazonDynamoDBConfig() {
+		return AmazonDynamoDBClientBuilder.standard()
+				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(SERVICE_ENDPOINT, REGION))
+				.withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(ACCESS_KEY, SECRET_KEY)))
+				.build();
+	}
+}
